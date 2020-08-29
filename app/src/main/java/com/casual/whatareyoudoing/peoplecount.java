13e5efdbef7c7ad25c26db608a1cda69e78ec138
prateekmahajan <mpsemailid@gmail.com>
2020-08-29 17:33:21 +0530

    page2 UI done

---
+++ app/src/main/java/com/casual/whatareyoudoing/peoplecount.java
@@ -0,0 +1,14 @@
+package com.casual.whatareyoudoing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class peoplecount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peoplecount2);
+    }
+}
